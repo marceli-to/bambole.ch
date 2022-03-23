1a166567eb3ecbefab12f17742801b3ecf832ee8
Marcel Stadelmann <m@marceli.to>
2022-03-23 19:46:16 +0100

    add manifest

--- resources/views/partials/content/manifest.blade.php
+++ resources/views/partials/content/manifest.blade.php
@@ -1,3 +1,3 @@
 <article class="theme-medium masonry-item">
-  <h2 class="text-light">Das Bambole vertritt klare Werte, die für die Organisator:innen und Helfer:innen genauso gelten wie für die Besucher:innen. Lade hier <a href="">unser Manifest</a> als PDF herunter</h2>
+  <h2 class="text-light">Das Bambole vertritt klare Werte, die für die Organisator:innen und Helfer:innen genauso gelten wie für die Besucher:innen. Lade hier <a href="/media/Manifest_Bambole.pdf" title="Download Bambole Manifest" target="_blank">unser Manifest</a> als PDF herunter</h2>
 </article>
