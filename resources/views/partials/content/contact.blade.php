7cebddc0dfd8cde3e8a2801d9d8915405cf6547c
Marcel Stadelmann <m@marceli.to>
2022-03-23 19:26:36 +0100

    staging build

--- resources/views/partials/content/contact.blade.php
+++ resources/views/partials/content/contact.blade.php
@@ -1,7 +1,11 @@
 <article class="theme-medium overlap masonry-item">
   <div>
     <h1>Kontakt</h1>
-    <p>Verein Bambole<br>Wolfensbergstrasse 59, 8400 Winterthur</p>
-    <p>Allgemeine Fragen: info@bambole.ch<br>Medienanfragen: pr@bambole.ch</p>
+    <p>Verein Bambole<br>Wolfensbergstrasse 59<br>8400 Winterthur</p>
+    <div class="align-center mb-1x md:mb-2x">Anfragen per E-Mail</div>
+    <p>
+      <a href="mailto:info@bambole.ch" title="Mail für allgemeine Fragen" class="mb-1x md:mb-2x">Allgemeine Fragen</a><br>
+      <a href="mailto:pr@bambole.ch" title="Mail für Medienanfragen">Medienanfragen</a>
+    </p>
   </div>
 </article>
