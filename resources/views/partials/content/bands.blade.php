77200fcac87c358c2c0d37b50c3e33d1697be67b
Marcel Stadelmann <m@marceli.to>
2022-03-23 19:44:48 +0100

    update

--- resources/views/partials/content/bands.blade.php
+++ resources/views/partials/content/bands.blade.php
@@ -2,6 +2,6 @@
   <div>
     <h1>Bands</h1>
     <p>Das Line-Up wird voraussichtlich Ende Mai / Anfang Juni bekannt.</p>
-    <p>Noch können sich interessierte Bands bewerben. Mailt an booking@bambole.ch.</p>
+    <p>Noch können sich interessierte Bands bewerben. Mailt an <a href="mailto:booking@bambole.ch" title="Mail an booking@bambole.ch">booking@bambole.ch</a>.</p>
   </div>
 </article>
