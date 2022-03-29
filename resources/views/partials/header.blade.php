74a60720c4f0984c6a4327398c3d6c3b73fe56c4
Marcel Stadelmann <m@marceli.to>
2022-03-29 09:20:24 +0200

    staging build

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,5 +3,6 @@
     @include('partials.misc.claim')
     @include('partials.misc.logo')
     @include('partials.misc.menu')
+    @include('partials.misc.interaction')
   </div>
 </header>
---
+++ resources/views/partials/misc/interaction.blade.php
@@ -0,0 +1,19 @@
+<nav class="interaction">
+  <ul>
+    <li>
+      <a href="https://www.facebook.com/bamboleopenair" title="Bambole Open Air on Facebook" target="_blank">
+        <img src="/assets/img/facebook.svg" height="40" width="40" alt="Facebook">
+      </a>
+    </li>
+    <li>
+      <a href="https://www.instagram.com/openair_bambole/" title="Bambole Open Air on Instagram" target="_blank">
+        <img src="/assets/img/instagram.svg" height="40" width="40" alt="Instagram">
+      </a>
+    </li>
+    <li>
+      <a href="mailto:info@bambole.ch" title="Mail an Bambole" target="_blank">
+        <img src="/assets/img/mail.svg" height="40" width="40" alt="Mail">
+      </a>
+    </li>
+  </ul>
+</nav> 
