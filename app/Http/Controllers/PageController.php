d8c6c987d5cf7625d7aa59f7b47dd3db6bf148c6
Marcel Stadelmann <m@marceli.to>
2022-05-29 17:30:27 +0200

    udpate

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -41,6 +41,42 @@ class PageController extends BaseController
   public function show(Band $band, $slug = NULL)
   {
     $band = Band::with('image', 'stage')->findOrFail($band->id);
-    return view($this->viewPath . 'show', ['band' => $band]);
+    return view($this->viewPath . 'show', ['band' => $band, 'browse' => $this->getBrowse($band)]);
   }
+
+  protected function getBrowse(Band $band)
+  {
+    $bands = Band::published()->orderBy('day')->orderBy('order')->get();
+    $keys = [];
+    foreach($bands as $b)
+    {
+      $keys[] = (int) $b->id;
+    }
+
+    $key = array_search($band->id, $keys);
+
+    if ($key == 0)
+    {
+      $prevId = end($keys);
+      $nextId = isset($keys[$key+1]) ? $keys[$key+1] : NULL;
+    }
+    else if ($key == count($keys) - 1)
+    {
+      $prevId = $keys[$key-1];
+      $nextId = $keys[0];
+    }
+    else
+    {
+      $prevId = $keys[$key-1];
+      $nextId = $keys[$key+1];
+    }
+
+    $items = [
+      'prev' => Band::find($prevId),
+      'next' => Band::find($nextId),
+    ];
+   
+    return $items;
+  }
+
 }
