84f3dfdcf4195b390980789afc0906b736848fc3
Marcel Stadelmann <m@marceli.to>
2022-05-31 14:22:45 +0200

    update

--- resources/views/partials/content/awarness.blade.php
+++ resources/views/partials/content/awarness.blade.php
@@ -9,6 +9,6 @@
   <p>Wir wollen Menschen ungeachtet ihres Geschlechts, ihrer Herkunft, ihrer Religion, ihrer Sexualität und ihrer sexuellen Identität ein tolles Erlebnis am Openair bieten.</p> 
   <p>Dazu gehört das Konzept <a href="https://diversityroadmap.org/genderisagalaxy/" target="_blank" title="Webseite Gender is a galaxy">Gender is a galaxy</a>, welches von den fantastischen Menschen von “Helvetia rockt” erarbeitet wurde. </p>
   <p>Auf anregende Weise werden unterschiedliche Geschlechterrollen und -identitäten thematisiert. Ein Teil unserer Toiletten wird genderneutral sein und mit den entsprechenden Postern von “Gender is a Galaxy” gekennzeichnet.</p>
-  <h3>Safe Space</h3>
-  <p>Wird es dir zuviel auf dem Festival? Wir bieten in Notsituationen einen Safe Space, den du Aufsuchen kannst wenn es dir mal zu viel wird. Melde dich an der Bar oder unseren Sanitäter:innen und wir schauen gemeinsam mit dir, was nötig ist.</p>
+  <h3>Safer Space</h3>
+  <p>Wird es dir zuviel auf dem Festival? Wir bieten in Notsituationen einen Safer Space, den du Aufsuchen kannst wenn es dir mal zu viel wird. Melde dich an der Bar oder unseren Sanitäter:innen und wir schauen gemeinsam mit dir, was nötig ist.</p>
 </article>
--- resources/views/partials/misc/logo.blade.php
+++ resources/views/partials/misc/logo.blade.php
@@ -1,3 +1,3 @@
 <div class="logo">
-  <img src="/assets/img/logo.png" width="1773" height="491" alt="Bambole">
+  <img src="/assets/img/logo-bg.png" width="1773" height="491" alt="Bambole">
 </div>
