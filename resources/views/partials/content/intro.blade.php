f30930d533dfeb98a80819eafa305d37b2387b45
Marcel Stadelmann <m@marceli.to>
2023-09-01 15:43:35 +0200

    wip

--- resources/views/partials/content/intro.blade.php
+++ resources/views/partials/content/intro.blade.php
@@ -4,5 +4,7 @@
   <h3>Bambole-Soli-Fest am 9.9.2023 im Gaswerk Winterthur,<br>Türöffnung 17 Uhr!</h3>
   <p>Wir erinnern uns: Am Freitagabend des Bambole 2022  legte ein fast schon apokalyptischer Starkregen den Festivalbetrieb während mehreren Stunden lahm. Teile der Infrastruktur wurden zerstört, und die Bars mussten schliessen! Das sorgte für ein sehr grosses Loch in unserer Kasse, das wir leider auch am sehr gut besuchten Samstag nicht mehr auffangen konnten. Und dann gabs ja  noch die obligate Coronaflaute.</p>
   <p>Deswegen organisieren wir am 9. September einen Soli-Anlass, mit dem Ziel, das Bambole auch 2024 im bewährten Rahmen durchführen zu können. Das Line-Up findet ihr <a href="#konzert" title="Line-Up">HIER</a>. Alles was ihr tun müsst, um uns zu supporten: Erzählt es weiter, kommt vorbei, rockt die Konzerte und genehmigt euch das eine oder andere Getränk! Alle Einnahmen fliessen vollumfänglich in ein unvergessliches Bambole 2024!</p>
+  <p>Verhungern müsst ihr übrigens auch nicht, es wird (auch vegan/vegetarisch) etwas zum mampfen geben.</p>
   <p>Tickets gibt es hier <a href="https://www.petzi.ch/de/events/53055-gaswerk-bambole-soli-fest" target="_blank" title="Tickets">hier</a>.</p>
+  <p>Preise: CHF 25, reduzierte Preise für Mitglieder Gaswerk und Inhaber:innen Kulturlegi, unter 16 Jahren freier Eintritt</p>
 </section>
