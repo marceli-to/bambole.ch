5e69a730805747d9b8a10bb29bbe363d23c4f654
Marcel Stadelmann <m@marceli.to>
2023-11-28 16:50:57 +0100

    wip

--- resources/views/partials/content/intro.blade.php
+++ resources/views/partials/content/intro.blade.php
@@ -1,6 +1,7 @@
 <section class="intro">
-  <h1>Das Bambole kehrt 2024 zurück!</h1>
+  <h1>Das Bambole kehrt am 2./3. August 2024 zurück!</h1>
   <p>Unterstütze unser rein ehrenamtlich organisierte Gratis-Openair gerne mit einer Spende, neu ganz einfach per Twint, Postfinance oder Kreditkarte! Mehr unter <a href="#spenden" title="Spenden">SPENDEN</a></p>
+  <p>Anfragen von Künstler:innen, die am nächsten Bambole auftreten möchten, nimmt unser Booking-Team gerne entgegen unter: <a href="mailto:booking@bambole.ch" title="Booking">booking@bambole.ch</a></p>
   {{-- <h3>Bambole-Soli-Fest am 9.9.2023 im Gaswerk Winterthur,<br>Türöffnung 17 Uhr!</h3>
   <p>Wir erinnern uns: Am Freitagabend des Bambole 2022  legte ein fast schon apokalyptischer Starkregen den Festivalbetrieb während mehreren Stunden lahm. Teile der Infrastruktur wurden zerstört, und die Bars mussten schliessen! Das sorgte für ein sehr grosses Loch in unserer Kasse, das wir leider auch am sehr gut besuchten Samstag nicht mehr auffangen konnten. Und dann gabs ja  noch die obligate Coronaflaute.</p>
   <p>Deswegen organisieren wir am 9. September einen Soli-Anlass, mit dem Ziel, das Bambole auch 2024 im bewährten Rahmen durchführen zu können. Das Line-Up findet ihr <a href="#konzert" title="Line-Up">HIER</a>. Alles was ihr tun müsst, um uns zu supporten: Erzählt es weiter, kommt vorbei, rockt die Konzerte und genehmigt euch das eine oder andere Getränk! Alle Einnahmen fliessen vollumfänglich in ein unvergessliches Bambole 2024!</p>
