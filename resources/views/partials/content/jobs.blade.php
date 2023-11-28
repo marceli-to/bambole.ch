928632a8fd8f181f2e1f89e5bfc817d3976bb1fc
Marcel Stadelmann <m@marceli.to>
2023-11-28 16:17:09 +0100

    wip

--- resources/views/partials/content/jobs.blade.php
+++ resources/views/partials/content/jobs.blade.php
@@ -1,10 +1,19 @@
 <article class="theme-medium overlap masonry-item" id="jobs">
   <div>
     <h1>Jobs</h1>
-    <p>Das Bambole sucht per sofort eine neue Infra-Leitung (ehrenamtlich!). Nächster geplant Event: August 2024. Planungsbeginn: Oktober 2023.</p>
-    <p><strong>DEINE AUFGABEN:</strong> Du koordinierst das bestehende Bauteam und ergänzt dieses durch neue Mitglieder. Indem Du in erster Linie administrative Arbeiten und Besorgungen erledigst, hältst Du dem Team den Rücken frei, damit es sich auf den Auf- und Abbau der Bars und Bühnen konzentrieren kann. In regelmässigen Sitzungen mit der Openair-Leitung (einmal monatlich, je näher der Event rückt selbsterklärend mehr) bestimmst Du die Zukunft Deines Ressorts und des Festivals an sich kreativ mit.</p>
-    <p><strong>WAS DU MITBRINGEN SOLLTEST:</strong> Handwerkliches Grundwissen, Kreativität, Freude an der Kultur und eine Einstellung, die zu unserem Manifest passt.</p>
-    <p><strong>WAS WIR BIETEN:</strong> Eine einzigartige Spielwiese mit grossem Lagersortiment, um sich selbst auszuprobieren (Fehler machen erlaubt!). Den Kontakt zu sympathischen, kulturaffinen Winterthurer:innen, somit auch für Neuzuzüger:innen geeignet. Einen Safe Space, wo Du einfach Du selbst sein kannst. Und die grosse Chance, Erfahrungen in der Eventbranche zu sammeln.</p>
-    <p><strong>INTERESSIERT?</strong> Melde Dich mit Deinen Fragen unverbindlich bei Chris <a href="mailto:mitmachen@bambole.ch" title="E-Mail an mitmachen@bambole.ch">mitmachen@bambole.ch</a> oder komm an unseren Stammtisch und lerne uns gleich persönlich kennen (idealerweise am 1. Juni oder 6. Juli, jeweils ab 19 Uhr im Widder Winterthur). Wir freuen uns!</p>
+    <p>Das Bambole sucht per sofort eine neue <strong>Leitung Ambiente</strong> (ehrenamtlich!) fürs Openair 2024 (und viele weitere).</p>
+    <p><strong>DEINE AUFGABEN:</strong> Du bist verantwortlich für die dekorative Gestaltung des Geländes auf dem Radhof. Dazu zählen Dekoelemente genauso wie Beschriftungen, Beleuchtung und Ausstattung. In Absprache mit den anderen OAL-Mitgliedern entscheidest du, wie das Bambole 2024 aussehen soll und kümmerst dich darum, dass die Vision Wirklichkeit wird.</p>
+    <p><strong>WAS DU MITBRINGEN SOLLTEST:</strong> Eine Menge kreativer Energie und Lust, das Openairgelände auf dem Radhof zu bespielen. Vorzugsweise warst du selber schon am Bambole und hast daher einen Eindruck vom Festival, der dich bei deinen Gestaltungsideen unterstützt. Du hast in der heissen Phase im Juli und Anfang August freie Zeit, in der du dich den Vorbereitungen und dem Auf- und Abbau widmen kannst. Du übernimmst Verantwortung für dein Ressort, kannst aber jederzeit auf Unterstützung aus der OAL und von Helfer:innen zählen.</p>
+    <p><strong>WAS WIR BIETEN:</strong> Ein Openairgelände mit wunderschöner Kulisse, auf dem du dich kreativ austoben kannst. Eine Scheune voller Material, das verbaut und verbastelt werden kann, dafür nur ein kleines Budget für Neuanschaffungen. Tatkräftige Helfer:innen und ein Team, das neuen Ideen und Menschen mit grosser Offenheit begegnet. 
+    <p><strong>INTERESSIERT?</strong> Melde Dich mit Deinen Fragen unverbindlich bei <a href="mailto:mitmachen@bambole.ch" title="E-Mail an mitmachen@bambole.ch">mitmachen@bambole.ch</a></p>
+    
+    <hr />
+
+    <p>Das Bambole sucht per sofort eine neue <strong>PR-Leitung</strong> (ehrenamtlich!). Nächster geplant Event: August 2024. Planungsbeginn: Sofort!</p>
+    <p><strong>DEINE AUFGABEN:</strong> Du bist für die Öffentlichkeitsarbeit rund um das Bambole zuständig. Du bewirbst das Openair, all seine Nebenanlässe sowie spezielle Aktionen auf originelle Art via Social Media, Homepage, Newsletter und den traditionellen Medien. Besucher:innen und Journalist:innen gibst Du bei Fragen Auskunft. Du lässt Plakate und Flyer gestalten, drucken und verteilst diese prominent. Am Anlass selbst koordinierst Du Fotograf:innen oder auch Videomacher:innen, um schöne Erinnerungen zu generieren.</p>
+    <p><strong>WAS DU MITBRINGEN SOLLTEST:</strong> Das Talent, verständliche Texte ohne viele Fehler zu verfassen, Social-Media-Grundwissen, Kreativität, Freude mit anderen Menschen zu kommunizieren, eine schnelle Reaktionszeit und starke Nerven, sollten auch einmal kritische Situationen entstehen, in welchen Du das Bambole nach aussen hin verteidigen musst.</p>
+    <p><strong>WAS WIR BIETEN:</strong> Bereits gut aufgebaute Social-Media-Plattformen zum Austoben und Ausprobieren. Den PR-Vorgänger, der Dir bei der Einarbeitung hilft. Den Kontakt zu sympathischen, kulturaffinen Winterthurer:innen, somit auch für Neuzuzüger:innen geeignet. Einen Safe Space, wo Du einfach Du selbst sein kannst. Und die grosse Chance, Erfahrungen in der Eventbranche zu sammeln.</p>
+    <p><strong>INTERESSIERT?</strong> Melde Dich mit Deinen Fragen unverbindlich bei <a href="mailto:mitmachen@bambole.ch" title="E-Mail an mitmachen@bambole.ch">mitmachen@bambole.ch</a></p>
+  
   </div>
 </article>
