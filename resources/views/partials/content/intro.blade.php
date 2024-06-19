5b69e2909450db94c477d6516130e00e4a56a3c4
Marcel Stadelmann <m@marceli.to>
2024-06-19 07:46:46 +0200

    wip

--- resources/views/partials/content/intro.blade.php
+++ resources/views/partials/content/intro.blade.php
@@ -1,4 +1,3 @@
 <section class="intro">
-  <h1>Das Bambole kehrt am 2./3. August 2024 zurück!</h1>
-  <p>Unterstütze unser rein ehrenamtlich organisierte Gratis-Openair gerne mit einer Spende, neu ganz einfach per Twint, Postfinance oder Kreditkarte! Mehr unter <a href="#spenden" title="Spenden">SPENDEN</a></p>
+  <h1>Das Bambole kehrt am <em>2./3. August 2024</em> zurück!</h1>
 </section>
