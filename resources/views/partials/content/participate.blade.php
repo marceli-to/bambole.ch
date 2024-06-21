f819b455549afc1667d6b6692b445622c45e3b0f
Marcel Stadelmann <m@marceli.to>
2024-06-21 12:11:51 +0200

    wip

--- resources/views/partials/content/participate.blade.php
+++ resources/views/partials/content/participate.blade.php
@@ -5,7 +5,7 @@
   <p><strong>Hier kommst DU ins Spiel:</strong> <a href="https://www.helfereinsatz.ch/de/bambole-openair-2024" title="Anmeldung Helfereinsatz" target="_blank" class="anchor anchor-light">Werde Teil</a> unseres Bambole-Teams! Nur so können wir garantieren, dass im August ein unvergessliches Festival stattfinden kann.</p>
   <p>Es gibt die unterschiedlichsten Aufgaben, die Du stunden- oder tageweise übernehmen kannst. Du musst hierfür kein Profi sein – Fehler machen und Erfahrungen sammeln ist bei uns ausdrücklich erlaubt.</p>
   <p><strong>Generell gilt:</strong> Helfer:innen für den Auf- und Abbau werden im Zeitraum zwischen dem 22. Juli und 5. August benötigt, jene während des Festivals vom 2./3. August.</p>
-  <p>Die Arbeit ist ehrenamtlich, Du wirst aber, je nach Einsatzdauer, vor Ort mit <a href="/media/bambole-helferinnenverguetung.pdf" title="Download Bambole Helfer*innnevergütung" target="_blank" class="anchor anchor-light">Gutscheinen</a> belohnt und nach dem Festival zu einem exklusiven Helfer:innen-Fest eingeladen.</p>
+  <p>Die Arbeit ist ehrenamtlich, Du wirst aber, je nach Einsatzdauer, vor Ort mit <a href="/media/bambole-helferinnenverguetung-2024.pdf" title="Download Bambole Helfer*innnevergütung" target="_blank" class="anchor anchor-light">Gutscheinen</a> belohnt und nach dem Festival zu einem exklusiven Helfer:innen-Fest eingeladen.</p>
   <h2>
     <a href="https://www.helfereinsatz.ch/de/bambole-openair-2024" target="_blank" title="Anmeldung Helfereinsatz" class="btn-primary">
       <span>Melde dich an!</span>
