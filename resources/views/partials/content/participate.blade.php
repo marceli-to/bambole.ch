76c55575b8dca3efa3a8a69ede81cb8f64798479
Marcel Stadelmann <m@marceli.to>
2024-07-17 13:28:22 +0200

    wip

--- resources/views/partials/content/participate.blade.php
+++ resources/views/partials/content/participate.blade.php
@@ -2,12 +2,12 @@
   <h1>Mach mit</h1>
   <h2>Ein Openair ohne Dich?<br>Ein No-Go</h2>
   <p>Das Bambole könnte nicht existieren <strong>ohne die zahlreichen ehrenamtlichen Helfer:innen</strong>, die uns bei der Vorbereitung, beim Auf- und Abbau und natürlich auch während des Festivals unterstützen.</p>
-  <p><strong>Hier kommst DU ins Spiel:</strong> <a href="https://www.helfereinsatz.ch/de/bambole-openair-2024" title="Anmeldung Helfereinsatz" target="_blank" class="anchor anchor-light">Werde Teil</a> unseres Bambole-Teams! Nur so können wir garantieren, dass im August ein unvergessliches Festival stattfinden kann.</p>
+  <p><strong>Hier kommst DU ins Spiel:</strong> <a href="https://portal.helfereinsatz.ch/bambole-openair-2024/" title="Anmeldung Helfereinsatz" target="_blank" class="anchor anchor-light">Werde Teil</a> unseres Bambole-Teams! Nur so können wir garantieren, dass im August ein unvergessliches Festival stattfinden kann.</p>
   <p>Es gibt die unterschiedlichsten Aufgaben, die Du stunden- oder tageweise übernehmen kannst. Du musst hierfür kein Profi sein – Fehler machen und Erfahrungen sammeln ist bei uns ausdrücklich erlaubt.</p>
   <p><strong>Generell gilt:</strong> Helfer:innen für den Auf- und Abbau werden im Zeitraum zwischen dem 22. Juli und 5. August benötigt, jene während des Festivals vom 2./3. August.</p>
   <p>Die Arbeit ist ehrenamtlich, Du wirst aber, je nach Einsatzdauer, vor Ort mit <a href="/media/bambole-helferinnenverguetung-2024.pdf" title="Download Bambole Helfer*innnevergütung" target="_blank" class="anchor anchor-light">Gutscheinen</a> belohnt und nach dem Festival zu einem exklusiven Helfer:innen-Fest eingeladen.</p>
   <h2>
-    <a href="https://www.helfereinsatz.ch/de/bambole-openair-2024" target="_blank" title="Anmeldung Helfereinsatz" class="btn-primary">
+    <a href="https://portal.helfereinsatz.ch/bambole-openair-2024/" target="_blank" title="Anmeldung Helfereinsatz" class="btn-primary">
       <span>Melde dich an!</span>
     </a>
   </h2>
