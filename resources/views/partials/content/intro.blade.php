be1f0092cc7be84134734cf22eaef69c90dfb2da
Marcel Stadelmann <m@marceli.to>
2025-05-06 10:25:53 +0200

    wip

--- resources/views/partials/content/intro.blade.php
+++ resources/views/partials/content/intro.blade.php
@@ -1,3 +1,4 @@
 <section class="intro">
-  <h1>Das Bambole kehrt am <em>2./3. August 2024</em> zurück!</h1>
+  {{-- <h1>Das Bambole kehrt am <em>2./3. August 2024</em> zurück!</h1> --}}
+  <h1>Das Bambole kehrt im <em>Sommer 2026</em> mit der letzten Ausgabe zurück. Mehr Infos bald!</h1>
 </section>
