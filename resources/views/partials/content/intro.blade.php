3b9e52bc538795624c40cf8cf4c2feb7b365a12c
Marcel Stadelmann <m@marceli.to>
2026-05-06 08:42:21 +0200

    Adds flyer warm up party

--- resources/views/partials/content/intro.blade.php
+++ resources/views/partials/content/intro.blade.php
@@ -2,5 +2,8 @@
   <h1>Das finale Bambole steigt vom <em>30. Juli</em> bis <em>1. August 2026!</em></h1>
   <p>Somit dauert die letzte Ausgabe einen Tag länger als in den Vorjahren. Freut euch auf gute Musik und ein abwechslungsreiches Rahmenprogramm. Auch an den Sonntagen vor und nach dem Event sind Überraschungen geplant.</p>
   <p>Mehr Infos folgen bald...</p>
-  <img src="/assets/img/bambole.jpg" width="1500" height="1000" alt="Bambole Logo">
+  {{-- <img src="/assets/img/bambole.jpg" width="1500" height="1000" alt="Bambole Logo"> --}}
+  <figure>
+    <img src="/assets/img/bambole-warm-up-party-30-05-2026.jpg" width="1440" height="1800" alt="Bambole Logo">
+  </figure>
 </section>
